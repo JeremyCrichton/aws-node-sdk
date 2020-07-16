b9bf16875472fc5447248e6cff5d412e06a7e4bd
Jeremy Crichton <22128243+JeremyCrichton@users.noreply.github.com>
2020-07-15 21:49:48 -0700

    update cloudfront deploy

---
+++ utils/cloudfront/createDistribution.js
@@ -0,0 +1,77 @@
+const AWS = require('aws-sdk');
+var uuid = require('uuid');
+
+const cloudfront = new AWS.CloudFront({ apiVersion: '2019-03-26' });
+
+// Create caller reference
+// @TODO: update prefix
+const callerReference = 'jade-' + uuid.v4();
+const originDomainName = 'test-bucket-jc-jul-15.s3.amazonaws.com';
+const originId = 'S3-test-bucket-jc-jul-15'; // unique ID of origin within the distribution
+
+const distParams = {
+  DistributionConfig: {
+    CallerReference: callerReference,
+    Comment: callerReference,
+    DefaultCacheBehavior: {
+      ForwardedValues: {
+        Cookies: {
+          Forward: 'none',
+          WhitelistedNames: {
+            Quantity: 0,
+          },
+        },
+        QueryString: false,
+        Headers: {
+          Quantity: 0,
+        },
+        QueryStringCacheKeys: {
+          Quantity: 0,
+        },
+      },
+      MinTTL: 0,
+      TargetOriginId: originId,
+      TrustedSigners: {
+        Enabled: false,
+        Quantity: 0,
+      },
+      ViewerProtocolPolicy: 'allow-all',
+      AllowedMethods: {
+        Items: ['GET', 'HEAD'],
+        Quantity: 2,
+        CachedMethods: {
+          Items: ['GET', 'HEAD'],
+          Quantity: 2,
+        },
+      },
+      Compress: false,
+      DefaultTTL: 86400,
+      LambdaFunctionAssociations: {
+        Quantity: 0,
+      },
+      MaxTTL: 31536000,
+      SmoothStreaming: false,
+    },
+    Enabled: true,
+    Origins: {
+      Items: [
+        {
+          DomainName: originDomainName,
+          Id: originId,
+          CustomOriginConfig: {
+            HTTPPort: 80,
+            HTTPSPort: 443,
+            OriginProtocolPolicy: 'match-viewer',
+          },
+        },
+      ],
+      Quantity: 1,
+    },
+  },
+};
+
+cloudfront.createDistribution(distParams, (err, data) => {
+  if (err) console.log(err, err.stack);
+  // an error occurred
+  else console.log(data); // successful response
+});
