1236581b227b36b6d69326cc6bfb06e832e78b1e
Jeremy Crichton <22128243+JeremyCrichton@users.noreply.github.com>
2020-07-15 19:07:13 -0700

    add upload to S3

--- utils/createBucket.js
+++ utils/createBucket.js
@@ -1,36 +1,24 @@
-// Load the SDK and UUID
+/**
+ * Create a bucket
+ * argv[2]: New Bucket Name
+ */
+
+// Load the AWS SDK for Node.js
 var AWS = require('aws-sdk');
-var uuid = require('uuid');
 
-// Create unique bucket name
-var bucketName = 'node-sdk-sample-' + uuid.v4();
-// Create name for uploaded object key
-var keyName = 'hello_world.txt';
+// Create S3 service object
+s3 = new AWS.S3({ apiVersion: '2006-03-01' });
 
-// Create a promise on S3 service object
-var bucketPromise = new AWS.S3({ apiVersion: '2006-03-01' })
-  .createBucket({ Bucket: bucketName })
-  .promise();
+// Create the parameters for calling createBucket
+var bucketParams = {
+  Bucket: process.argv[2],
+};
 
-// Handle promise fulfilled/rejected states
-bucketPromise
-  .then(function (data) {
-    // Create params for putObject call
-    var objectParams = {
-      Bucket: bucketName,
-      Key: keyName,
-      Body: 'Hello World!',
-    };
-    // Create object upload promise
-    var uploadPromise = new AWS.S3({ apiVersion: '2006-03-01' })
-      .putObject(objectParams)
-      .promise();
-    uploadPromise.then(function (data) {
-      console.log(
-        'Successfully uploaded data to ' + bucketName + '/' + keyName
-      );
-    });
-  })
-  .catch(function (err) {
-    console.error(err, err.stack);
-  });
+// call S3 to create the bucket
+s3.createBucket(bucketParams, function (err, data) {
+  if (err) {
+    console.log('Error', err);
+  } else {
+    console.log('Success', data.Location);
+  }
+});
---
+++ utils/uploadToBucket.js
@@ -0,0 +1,35 @@
+/**
+ * Upload a file to an existing bucket
+ * argv[2]: BUCKET_NAME
+ * argv[3]: FILE_NAME
+ */
+
+// Load the AWS SDK for Node.js
+var AWS = require('aws-sdk');
+
+// Create S3 service object
+s3 = new AWS.S3({ apiVersion: '2006-03-01' });
+
+// call S3 to retrieve upload file to specified bucket
+var uploadParams = { Bucket: process.argv[2], Key: '', Body: '' };
+var file = process.argv[3];
+
+// Configure the file stream and obtain the upload parameters
+var fs = require('fs');
+var fileStream = fs.createReadStream(file);
+fileStream.on('error', function (err) {
+  console.log('File Error', err);
+});
+uploadParams.Body = fileStream;
+var path = require('path');
+uploadParams.Key = path.basename(file);
+
+// call S3 to retrieve upload file to specified bucket
+s3.upload(uploadParams, function (err, data) {
+  if (err) {
+    console.log('Error', err);
+  }
+  if (data) {
+    console.log('Upload Success', data.Location);
+  }
+});
